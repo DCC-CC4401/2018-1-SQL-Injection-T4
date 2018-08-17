4e6955ed2c7c2d722c5b4f6884deefbba12e4444
cristianfloresm <cristian.flores.m@ing.uchile.cl>
2018-08-17 14:27:27 +0000

    Horario y días hábiles para la reserva de artículos

--- cc4401Inventory/articlesApp/views.py
+++ cc4401Inventory/articlesApp/views.py
@@ -47,7 +47,7 @@ def article_data(request, article_id):
 def verificar_horario_habil(horario):
     if horario.isocalendar()[2] > 5:
         return False
-    if horario.hour < 9 or horario.hour > 18:
+    if horario.hour < 9 or (horario.hour == 18 and horario.minute > 0) or horario.hour > 18:
         return False
 
     return True
@@ -71,7 +71,7 @@ def article_request(request):
                     messages.warning(request, 'Los pedidos deben ser hechos al menos con una hora de anticipación.')
                 elif start_date_time.date() != end_date_time.date():
                     messages.warning(request, 'Los pedidos deben ser devueltos el mismo día que se entregan.')
-                elif not verificar_horario_habil(start_date_time) and not verificar_horario_habil(end_date_time):
+                elif not verificar_horario_habil(start_date_time) or not verificar_horario_habil(end_date_time):
                     messages.warning(request, 'Los pedidos deben ser hechos en horario hábil.')
                 else:
                     loan = Loan(article=article, starting_date_time=start_date_time, ending_date_time=end_date_time,
