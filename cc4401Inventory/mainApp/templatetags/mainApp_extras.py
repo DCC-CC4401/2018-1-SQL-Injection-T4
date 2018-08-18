358be29cd3eac29475697e2fb6e6d4c7c5dcf642
Jhonny Cerezo <jhoncc2@gmail.com>
2018-08-18 02:01:41 -0300

    Add selectable jquery buttons, and add time-related information in grid/calendar

--- cc4401Inventory/mainApp/templatetags/mainApp_extras.py
+++ cc4401Inventory/mainApp/templatetags/mainApp_extras.py
@@ -1,4 +1,5 @@
 from django.template.defaulttags import register
+from datetime import datetime, timedelta, date
 
 @register.filter
 def get_item(dictionary, key):
@@ -7,3 +8,13 @@ def get_item(dictionary, key):
 @register.filter
 def zipWith(a, b):
     return zip(a,b)
+
+
+@register.filter
+def weekDayToDate(day, weekDate):
+    return (datetime.strptime(weekDate, "%d/%m/%Y") + timedelta(days=int(day))).strftime("%d/%m/%Y")
+     
+
+@register.filter
+def dayNumberToWeekDay(day, weekDate):
+    return weekDate[int(day)]
