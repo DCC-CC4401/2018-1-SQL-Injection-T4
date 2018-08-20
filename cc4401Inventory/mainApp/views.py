6a69e700f3340ae20a8a94bb7b06b4bbdfe11e5d
Victor <victor.garrido.u@gmail.com>
2018-08-20 20:33:03 -0300

    ahora se puede buscar por fecha y por estado solo

--- cc4401Inventory/mainApp/views.py
+++ cc4401Inventory/mainApp/views.py
@@ -14,10 +14,9 @@ def landing_articles(request):
 
 @login_required
 def landing_spaces(request, date=None):
-
     if date:
         current_date = date
-        current_week = datetime.datetime.strptime(current_date,"%Y-%m-%d").date().isocalendar()[1]
+        current_week = datetime.datetime.strptime(current_date, "%Y-%m-%d").date().isocalendar()[1]
     else:
         try:
             current_week = datetime.datetime.strptime(request.GET["date"], "%Y-%m-%d").date().isocalendar()[1]
@@ -26,7 +25,7 @@ def landing_spaces(request, date=None):
             current_week = datetime.date.today().isocalendar()[1]
             current_date = datetime.date.today().strftime("%Y-%m-%d")
 
-    reservations = Reservation.objects.filter(starting_date_time__week = current_week, state__in = ['P','A'])
+    reservations = Reservation.objects.filter(starting_date_time__week=current_week, state__in=['P', 'A'])
     colores = {'A': 'rgba(0,153,0,0.7)',
                'P': 'rgba(51,51,204,0.7)'}
 
@@ -39,20 +38,25 @@ def landing_spaces(request, date=None):
         reserv.append(localtime(r.starting_date_time).strftime("%H:%M"))
         reserv.append(localtime(r.ending_date_time).strftime("%H:%M"))
         reserv.append(colores[r.state])
-        res_list[r.starting_date_time.isocalendar()[2]-1].append(reserv)
+        res_list[r.starting_date_time.isocalendar()[2] - 1].append(reserv)
 
     move_controls = list()
-    move_controls.append((datetime.datetime.strptime(current_date,"%Y-%m-%d")+datetime.timedelta(weeks=-4)).strftime("%Y-%m-%d"))
-    move_controls.append((datetime.datetime.strptime(current_date,"%Y-%m-%d")+datetime.timedelta(weeks=-1)).strftime("%Y-%m-%d"))
-    move_controls.append((datetime.datetime.strptime(current_date,"%Y-%m-%d")+datetime.timedelta(weeks=1)).strftime("%Y-%m-%d"))
-    move_controls.append((datetime.datetime.strptime(current_date,"%Y-%m-%d")+datetime.timedelta(weeks=4)).strftime("%Y-%m-%d"))
+    move_controls.append(
+        (datetime.datetime.strptime(current_date, "%Y-%m-%d") + datetime.timedelta(weeks=-4)).strftime("%Y-%m-%d"))
+    move_controls.append(
+        (datetime.datetime.strptime(current_date, "%Y-%m-%d") + datetime.timedelta(weeks=-1)).strftime("%Y-%m-%d"))
+    move_controls.append(
+        (datetime.datetime.strptime(current_date, "%Y-%m-%d") + datetime.timedelta(weeks=1)).strftime("%Y-%m-%d"))
+    move_controls.append(
+        (datetime.datetime.strptime(current_date, "%Y-%m-%d") + datetime.timedelta(weeks=4)).strftime("%Y-%m-%d"))
 
-    delta = (datetime.datetime.strptime(current_date, "%Y-%m-%d").isocalendar()[2])-1
-    monday = ((datetime.datetime.strptime(current_date, "%Y-%m-%d") - datetime.timedelta(days=delta)).strftime("%d/%m/%Y"))
-    context = {'reservations' : res_list,
-               'current_date' : current_date,
-               'controls' : move_controls,
-               'actual_monday' : monday}
+    delta = (datetime.datetime.strptime(current_date, "%Y-%m-%d").isocalendar()[2]) - 1
+    monday = (
+        (datetime.datetime.strptime(current_date, "%Y-%m-%d") - datetime.timedelta(days=delta)).strftime("%d/%m/%Y"))
+    context = {'reservations': res_list,
+               'current_date': current_date,
+               'controls': move_controls,
+               'actual_monday': monday}
     return render(request, 'espacios.html', context)
 
 
@@ -61,11 +65,11 @@ def landing_search(request, products):
     if not products:
         return landing_articles(request)
     else:
-        context = {'productos' : products,
-                   'colores' : {'D': '#009900',
-                                'R': '#ffcc00',
-                                'P': '#3333cc',
-                                'L': '#cc0000'}
+        context = {'productos': products,
+                   'colores': {'D': '#009900',
+                               'R': '#ffcc00',
+                               'P': '#3333cc',
+                               'L': '#cc0000'}
                    }
         return render(request, 'articulos.html', context)
 
@@ -74,13 +78,12 @@ def landing_search(request, products):
 def search(request):
     if request.method == "GET":
         query = request.GET['query']
-        #a_type = "comportamiento_no_definido"
-        a_state = "A" if (request.GET['estado'] == "A") else request.GET['estado']
-
+        a_state = request.GET['estado']
+        date = request.GET['fecha']
+        articles = Article.objects.all()
         if not (a_state == "A"):
-            articles = Article.objects.filter(state=a_state,name__icontains=query.lower())
-        else:
-            articles = Article.objects.filter(name__icontains=query.lower())
-
-        products = None if (request.GET['query'] == "") else articles
-        return landing_search(request, products)
+            articles = articles.filter(state=a_state)
+        if not (query == ''):
+            print("banana")
+            articles = articles.filter(name__icontains=query.lower())
+        return landing_search(request, articles)
