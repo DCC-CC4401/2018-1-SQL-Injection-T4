6778c0909a5a55352d73238339161cb30b6b8958
maximiliano edwards <maximiliano.edwards@ing.uchile.cl>
2018-08-21 12:20:54 -0300

    ficha reserva y ficha préstamo completas

--- cc4401Inventory/cc4401Inventory/urls.py
+++ cc4401Inventory/cc4401Inventory/urls.py
@@ -26,4 +26,5 @@ urlpatterns = [
     path('article/', include('articlesApp.urls')),
     path('space/', include('spacesApp.urls')),
     path('reservation/', include('reservationsApp.urls')),
+    path('loan/', include('loansApp.urls')),
 ] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
